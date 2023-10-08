12a7502b672dcd12ae031d69de1a38f2d90e1663
Hashim <hashimabdulmujib@gmail.com>
2023-10-08 08:54:25 +0100

    add the abstraction of infrastructure to Domain layer then install FluentValidation

---
+++ src/CapitalPlacement.Domain/Infrastructure/Respository/ICPProgramRepository.cs
@@ -0,0 +1,11 @@
+﻿using CapitalPlacement.Domain.Entities;
+
+namespace CapitalPlacement.Domain.Infrastructure.Respository
+{
+    public interface ICPProgramRepository
+    {
+        Task<CPProgram> GetByIdAsync(Guid id);
+        Task<CPProgram> CreateAsync(CPProgram item);
+        Task UpdateAsync(CPProgram item);
+    }
+}
