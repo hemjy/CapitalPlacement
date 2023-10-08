68da8ab26fbc1dad086f1f352a99de93b3588abe
Hashim <hashimabdulmujib@gmail.com>
2023-10-08 14:45:58 +0100

    write additional test

--- src/CapitalPlacement.Test/IntegrationTests/ProgramDetailControllerTest.cs
+++ src/CapitalPlacement.Test/IntegrationTests/ProgramDetailControllerTest.cs
@@ -1,11 +1,13 @@
 ﻿
 
 using CapitalPlacement.API.Controllers;
+using CapitalPlacement.Application.Common.Models;
 using CapitalPlacement.Application.DTOs;
 using CapitalPlacement.Test.Helper;
 using CapitalPlacement.Test.IntegrationTests.Base;
 using Newtonsoft.Json;
 using System.Net;
+using System.Net.Http.Json;
 using System.Text;
 using Xunit;
 
@@ -42,6 +44,34 @@ namespace CapitalPlacement.Test.IntegrationTests
             // Assert: Check the response
             Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
 
+        }
+
+        [Fact]
+        public async Task Get_Should_Return_OkResult()
+        {
+            // Act
+            var response = await _client.GetAsync($"/api/program/{_programId}");
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+             var result = await response.Content.ReadFromJsonAsync<Result<ProgramDetailDTO>>();
+             Assert.NotNull(result);
+             Assert.True(result.Succeeded);
+             Assert.NotEmpty(result.Data.Title);
+        }
+
+        [Fact]
+        public async Task Get_Should_Return_ErrorResult()
+        {
+            var programId = Guid.NewGuid();
+            // Act
+            var response = await _client.GetAsync($"/api/program/{programId}");
+
+            // Assert
+            var programDetails = await response.Content.ReadFromJsonAsync<Result<ProgramDetailDTO>>();
+            Assert.NotNull(programDetails);
+            Assert.False(programDetails.Succeeded);
         }
     }
 }
