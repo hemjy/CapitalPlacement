851a62b9e2ac52a10c9af252b13fc4882e7024ee
Hashim <hashimabdulmujib@gmail.com>
2023-10-08 10:00:19 +0100

    add configuration settings

---
+++ src/CapitalPlacement.API/Extensions/ConfigurationExtension.cs
@@ -0,0 +1,22 @@
+﻿using CapitalPlacement.Application;
+using CapitalPlacement.Application.Services;
+using CapitalPlacement.Domain.Infrastructure.Respository;
+using CapitalPlacement.Domain.Services;
+using CapitalPlacement.Infrastructure.Repositories;
+using System.Reflection;
+
+namespace CapitalPlacement.API.Extensions
+{
+    public static class ConfigurationExtension
+    {
+        public static void AddProgramServices(this IServiceCollection services)
+        {
+            services.AddInfrastructureServices();
+            services.AddApplicationServices();
+        }
+        public static void AddInfrastructureServices(this IServiceCollection services)
+        {
+           services.AddScoped<ICPProgramRepository, CPProgramRepository>();
+        }
+    }
+}
--- src/CapitalPlacement.API/Program.cs
+++ src/CapitalPlacement.API/Program.cs
@@ -1,11 +1,26 @@
+using CapitalPlacement.API.Extensions;
+using CapitalPlacement.Middlewares;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddFluentValidation(x =>
+     {
+         x.AutomaticValidationEnabled = true;
+
+     });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddProgramServices();
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.SuppressModelStateInvalidFilter = false;
+});
 
 var app = builder.Build();
 
@@ -15,7 +30,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseMiddleware<ErrorHandlerMiddleware>();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
